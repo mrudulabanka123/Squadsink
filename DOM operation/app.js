07f55e6488b1d2dd1b5ef3ee13726a645f6875e7
mrudulabanka123 <mrudhulabanka@gmail.com>
2025-08-28 23:00:04 +0530

    DOM Attribute Manipulations

--- DOM operation/app.js
+++ DOM operation/app.js
@@ -1,3 +1,4 @@
+//Create an Elements:
 const bootstrapIteam=document.createElement("li");
 bootstrapIteam.innerHTML= '<a href:"https://getbootstrap.com/">Bootstrap</a>';
 
@@ -37,3 +38,40 @@ Java8Iteam.innerText="java 8";
 
 //  pythonIteam.textContent="TypeScript"
  pythonIteam.replaceWith("TypeScript");
+
+  const nodejs=document.createElement("li");
+  nodejs.innerText="Nodejs";
+  courseList.append(nodejs);
+  courseList.insertBefore(nodejs,tailwindIteam);
+  reactIteam.insertAdjacentElement("beforeend",nodejs);
+  nodejs.textContent="Node.js";
+  
+  courseList.insertBefore(bootstrapIteam,tailwindIteam);
+   const bootstrapIteamjs=document.createElement("li");
+   bootstrapIteamjs.innerText="bootstrapjs";
+  courseList.replaceChild(bootstrapIteamjs,bootstrapIteam);
+  bootstrapIteamjs.replaceWith("bootstrapIteam");
+//create a profile picture Element
+  const profilePicture=document.createElement("img");
+  profilePicture.setAttribute("src","https://images.pexels.com/photos/33511055/pexels-photo-33511055.jpeg");
+  profilePicture.setAttribute("alt","profile Imge");
+//class Manipulations
+  profilePicture.className="profilePicture";
+  profilePicture.classList.add("ceoImage");
+  profilePicture.classList.remove("ceoImage");
+  profilePicture.classList.toggle("highlight");
+  //Append profile picture to contaner
+  const profileContainer=document.getElementById("container");
+  profileContainer.append(profilePicture);
+
+  const navelink=document.createElement("a");
+  navelink.href="https://www.achiversit.com";
+  navelink.innerText="Visit AchiversIT";
+//   navelink.target="_blank";
+  profileContainer.append(navelink);
+
+
+  const getExistingpath=document.getElementById("container");
+  console.log(getExistingpath);
+
+  console.log(getExistingpath.getAttribute("smallContainer"))
